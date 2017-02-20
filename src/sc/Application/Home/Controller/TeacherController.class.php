9ef9bd2c85039b5bb1727ed4a5e89e2edb2c54d0
haylee <haylhf1987@hotmail.com>
2017-02-21 00:30:58 +0800

    add some logic judgment

--- src/sc/Application/Home/Controller/TeacherController.class.php
+++ src/sc/Application/Home/Controller/TeacherController.class.php
@@ -82,15 +82,75 @@ class TeacherController extends BaseLoginController
         $this->display();
     }
 
+    //同一个专业都为必修课时，验证是否有时间冲突
+    public function isConflictCourseTime($data)
+    {
+        $result = false;
+        $condition['profession_id'] = $data['profession_id'];
+        $condition['course_weekday'] = $data['course_weekday'];
+        $condition['course_start_time'] = $data['course_start_time'];
+        $condition['course_type'] = $data['course_type'];
+        if ($condition['course_type'] == 0)//必修
+        {
+            $items = D("TeacherCourse")->getRecords($condition);
+            $result = count($items) > 0;
+        }
+        return $result;
+    }
+
+    //验证同一时间上课地点是否有冲突
+    public function isConflictCourseBuilding($data)
+    {
+        $result = false;
+        $condition['building_id'] = $data['building_id'];
+        $condition['course_weekday'] = $data['course_weekday'];
+        $condition['course_start_time'] = $data['course_start_time'];
+        $items = D("TeacherCourse")->getRecords($condition);
+        $result = count($items) > 0;
+        return $result;
+    }
+
+    //验证同一个必修课在同一个专业只能是一个教师上
+    public function isConflictCourse($data)
+    {
+        $result = false;
+        $condition['profession_id'] = $data['profession_id'];
+        $condition['course_id'] = $data['course_id'];
+        $condition['course_type'] = $data['course_type'];
+        if ($condition['course_type'] == 0)//必修
+        {
+            $items = D("TeacherCourse")->getRecords($condition);
+            $result = count($items) > 0;
+        }
+        return $result;
+    }
+
     public function ajaxAddTeacherCourse()
     {
-        $result = null;
+        $result = false;
+        $msg = '';
+        if ($this->isConflictCourse($_POST)) {
+            $msg = '您所开的必修课程在所在专业已经有其它老师开设了，请重新选择';
+            $this->error($msg);
+            return;
+        }
+        if ($this->isConflictCourseTime($_POST)) {
+            $msg = '您所开的必修课程在所在专业与其它课程时间冲突，请重新选择';
+            $this->error($msg);
+            return;
+        }
+        if ($this->isConflictCourseBuilding($_POST)) {
+            $msg = '您所开课程与其它课程在同一时间地点，请重新选择';
+            $this->error($msg);
+            return;
+        }
         $result = D("TeacherCourse")->addData($_POST);
         if ($result) {
             $this->success(true);
         } else {
             $this->error('error');
         }
+
     }
 
     public function ajaxDeleteRecord()
--- src/sc/Application/Home/Model/StudentCourseModel.class.php
+++ src/sc/Application/Home/Model/StudentCourseModel.class.php
@@ -51,33 +51,38 @@ class StudentCourseModel extends Model
         if ($condition) {
             if (isset($condition['student_id'])) {
                 $str = $condition['student_id'];
-                $data .= " AND course_id NOT IN 
+                $data .= " AND v.course_id NOT IN 
                 (SELECT sc.course_id 
                 FROM sc_student_course_view AS sc
                 WHERE sc.student_id = '$str'
-                )  ";
+                )  ";//排除已经选择的课程
             }
             if ($condition['teacher_id']) {
                 $str = $condition['teacher_id'];
-                $data .= " AND teacher_id='$str' ";
+                $data .= " AND v.teacher_id='$str' ";
             }
             if ($condition['course_id']) {
                 $str = $condition['course_id'];
-                $data .= " AND course_id='$str' ";
+                $data .= " AND v.course_id='$str' ";
             }
             if ($condition['profession_id']) {
                 $str = $condition['profession_id'];
-                $data .= " AND profession_id='$str' ";
+                $data .= " AND v.profession_id='$str' ";
             }
             if (isset($condition['course_type']) && $condition['course_type'] >= 0) {
                 $str = $condition['course_type'];
-                $data .= " AND course_type='$str' ";
+                $data .= " AND v.course_type='$str' ";
             }
         }
         $list = M('teacher_course_department_view')
 //            ->fetchSql(true)// test sql
-            ->alias("s")
+            ->alias("v")
+            ->join('LEFT JOIN 
+            (SELECT teacher_course_id, COUNT(*) AS selected_people FROM ' . C('DB_PREFIX') .
+            'student_course GROUP BY teacher_course_id) AS sp 
+	        ON sp.teacher_course_id=v.teacher_course_id')//列出该课程的已选人数
             ->where($data)
+            ->field('v.*,sp.selected_people')
             ->select();
 //        echo $list;
         if (!isset($list)) {
